826dd21350449acef8bc4fac6364c7142811954d
Tesla2313 <66022719+Tesla2313@users.noreply.github.com>
2020-05-29 18:52:17 +0300

    Add files via upload

---
+++ core/src/main/java/dto/UserDataDTO.java
@@ -0,0 +1,49 @@
+package dto;
+
+import io.swagger.annotations.ApiModelProperty;
+import model.Role;
+
+public class UserDataDTO {
+
+    //Fixed role as client
+    Role role = Role.ROLE_CLIENT;
+    @ApiModelProperty(position = 0)
+    private String username;
+    @ApiModelProperty(position = 1)
+    private String email;
+    @ApiModelProperty(position = 2)
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+}
---
+++ core/src/main/java/websocket/MyStompSessionHandler.java
@@ -0,0 +1,66 @@
+package websocket;
+
+import com.google.gson.Gson;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaders;
+import org.springframework.messaging.simp.stomp.StompSession;
+import org.springframework.messaging.simp.stomp.StompSessionHandler;
+
+import java.io.BufferedWriter;
+import java.lang.reflect.Type;
+import java.util.List;
+import java.util.Queue;
+
+@SuppressWarnings("NullableProblems")
+public class MyStompSessionHandler implements StompSessionHandler {
+    private static final Logger logger = LoggerFactory.getLogger(MyStompSessionHandler.class);
+    private final Queue<DefaultSTOMPMessage> receivedMessageQueue;
+    private final Gson gson;
+    private List<String> subs;
+    private BufferedWriter output;
+
+    public MyStompSessionHandler(List<String> subs, Queue<DefaultSTOMPMessage> queue) {
+        this.subs = subs;
+        this.receivedMessageQueue = queue;
+        this.gson = new Gson();
+    }
+
+    public MyStompSessionHandler(List<String> subs, Queue<DefaultSTOMPMessage> queue, BufferedWriter output) {
+        this(subs, queue);
+        this.output = output;
+    }
+
+    @Override
+    public void afterConnected(StompSession session, StompHeaders connectedHeaders) {
+        for (String sub : subs) {
+            session.subscribe(sub, this);
+        }
+    }
+
+    @Override
+    public void handleException(StompSession session, StompCommand command, StompHeaders headers, byte[] payload, Throwable exception) {
+        logger.error(String.format("handleException [%s] with payload [%s]", exception.getMessage(), new String(payload)));
+    }
+
+    @Override
+    public void handleTransportError(StompSession session, Throwable exception) {
+        logger.error(String.format("handleTransportError [%s] with session [%s]", exception.getMessage(), session.toString()));
+    }
+
+    @Override
+    public Type getPayloadType(StompHeaders headers) {
+        return DefaultSTOMPMessage.class;
+    }
+
+    @Override
+    public void handleFrame(StompHeaders headers, Object payload) {
+        DefaultSTOMPMessage msg = (DefaultSTOMPMessage) payload;
+        msg.setAckID(headers.getAck());
+
+        if (!receivedMessageQueue.offer(msg)) {
+            logger.warn("Queue of received STOMP messages is full! Discarding messages!");
+        }
+    }
+}
